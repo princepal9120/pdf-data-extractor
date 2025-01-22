23c7c9a6586386c0d1e21e284e444e61d22d9374
Prince Pal <princepal9120@gmail.com>
2025-01-23 03:44:40 +0530

    cors error

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const path = require('path')
 const app = express();
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "https://pdf-data-extractor-tjdv.onrender.com"],
         methods: ["GET", "POST"],
         credentials: true,
     })
